21563ccb87e41b5a1f3fcb0c2366008f341c6365
Khalil <khalilhenane@hotmail.com>
2025-01-26 14:05:58 +0100

    adjusted the introduction text

--- lib/onboarding/pages/introduction.dart
+++ lib/onboarding/pages/introduction.dart
@@ -5,7 +5,7 @@ import 'package:line_icons/line_icons.dart';
 PageViewModel startPage() {
   return PageViewModel(
     title: "Welcome to FoodPick",
-    body: "Your favorite restaurants, just a tap away. Order delicious meals from the best local spots.",
+    body: "Expand your reach and grow your business by connecting with more customers. Showcase your delicious meals and attract food lovers .",
     image: Icon(
       Icons.restaurant,
       size: 150.0,
