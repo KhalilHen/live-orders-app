de44f2e8cc136115848ca32e15628db71e68e2a8
Khalil <khalilhenane@hotmail.com>
2025-01-27 09:38:19 +0100

    Made a bit of a mock-up of the dialog of the strucutre

---
+++ lib/models/enum/restaurant.dart
@@ -0,0 +1,16 @@
+// // import '';
+
+
+// class Restaurant {
+//   final int id;
+//   final String name;
+//   final String description;
+//   final double rating;
+//   final int reviewCount;
+//   final String imageUrl;  WidgetsFlutterBinding.ensureInitialized();
+//   await Supabase.initialize(
+//     url: 'https://lhytubtnsyzovyfrsqpm.supabase.co',
+//     anonKey:
+//         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoeXR1YnRuc3l6b3Z5ZnJzcXBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzMTI0NzMsImV4cCI6MjA1MDg4ODQ3M30.N0_6Mk-r5H49vE26j7vze6NmezIqcclA0EPAlNaS0xs',
+//   );
+// }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -8,10 +8,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  //For searching
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Incoming Orders",
+          //*Incoming Orders, or Orders not sure yet about the title.
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return showOrderAlert();
+              },
+            );
+          },
+          child: Text("Show dialog"),
+        ),
+      ),
+    );
+  }
+
+  AlertDialog showOrderAlert() {
+    return AlertDialog(
+      title: Text("Incoming Order"),
+      content: Text("New incomming order do you want to accept o cancel this order?"),
+      actions: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [ElevatedButton(onPressed: null, child: Text("Cancel")), ElevatedButton(onPressed: null, child: Text("Accept"))],
+        )
+      ],
+    );
   }
 }
