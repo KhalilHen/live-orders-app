02097ba9c3ef01c86ff70f23d9172a43631844fd
Khalil <khalilhenane@hotmail.com>
2025-01-29 20:04:28 +0100

    Added total stats title, with more tiles

--- lib/pages/stats_page.dart
+++ lib/pages/stats_page.dart
@@ -23,83 +23,136 @@ class _StatsPageState extends State<StatsPage> {
           style: Theme.of(context).appBarTheme.titleTextStyle,
         ),
       ),
-      body: Column(
-
-
-        children: [
-          Padding(padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-
-            children: [
-              //Chart con
-              Column(
-children: [
-
-  const Text("Restaurant growth", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-              const SizedBox(height: 16,),
-                 //This the graph container
-              Row(
-children: [
-                   //TODO Work here late ron
-    //* Container to simulate the graph space
-                   Container(
-                    height: 300,
-                    width: 700,
-                    color: Colors.red,
-                   )
-                  //   AspectRatio(
-                  //   aspectRatio: 2.0,
-                  //   child: LineChart(
-                  //     LineChartData(
-                  
-                        
-                  //     ),
-                  //   ),
-                  // ),
-
-]
-
-              )
-
-              
-]
-            
-
-              )
-
-            ,
-         
-            ],
-          ),
-          )
-,
-          const Text("Today\s overview", style: TextStyle(
-             fontSize: 20, fontWeight: FontWeight.bold,
-
-          ),),
-
-          const SizedBox(height: 16,),
-          Row(
-
+      body: SingleChildScrollView(
+        child: Column(
+        
+        
           children: [
-            //tried importing the custom widget 
-            // statistikCard()
-            staticCard( "Today\s Orders ", "60", Icons.receipt_long, Colors.deepOrange),
-              const SizedBox(width: 16,)
-            , staticCard( "Today\s Revenue ", "€30", Icons.payment, Colors.green),
-          ],  
+            Padding(padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+        
+              children: [
+                //Chart con
+                Column(
+        children: [
+        
+          const Text("Restaurant growth", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                const SizedBox(height: 16,),
+                   //This the graph container
+                Row(
+        children: [
+                     //TODO Work here late ron
+            //* Container to simulate the graph space
+                     Container(
+                      height: 300,
+                      width: 700,
+                      color: Colors.red,
+                     )
+                    //   AspectRatio(
+                    //   aspectRatio: 2.0,
+                    //   child: LineChart(
+                    //     LineChartData(
+                    
+                          
+                    //     ),
+                    //   ),
+                    // ),
+        
+        ]
+        
+                )
+        
+                
+        ]
+              
+        
+                )
+        
+              ,
+           
+              ],
+            ),
+            )
+        ,
+            const Text("Today\s overview", style: TextStyle(
+               fontSize: 20, fontWeight: FontWeight.bold,
+        
+            ),),
+        
+            const SizedBox(height: 16,),
+            Row(
+        
+            children: [
+              //tried importing the custom widget 
+              // statistikCard()
+              staticCard( "Today\s Orders ", "60", Icons.receipt_long, Colors.deepOrange),
+                const SizedBox(width: 16,)
+              , staticCard( "Today\s Revenue ", "€30", Icons.payment, Colors.green),
+            ],  
+        
+        
+               
+            )
+        ,
+            Row(children: [
+              staticCard( "Avg Order Value ", "€10", Icons.trending_up, Colors.blue),
+                const SizedBox(width: 12,)
+              , staticCard( "Active orders ", "10", Icons.pending_actions, Colors.orange),
+            ],),
+        
+            // ,Padding(padding: const EdgeInsets.all(16),)
+            SizedBox(height: 30,)
+        
+        ,
+            Text("Total stats", style: TextStyle(
+               fontSize: 20, fontWeight: FontWeight.bold,
+        
+        
+            ),),
 
-
-             
-          )
+            const SizedBox(height: 16,),
+            Row(children: [
+        
+              staticCard("Total Orders", "700", Icons.receipt_long, Colors.deepOrange),
+                            const SizedBox(width: 12,)
 ,
-          Row(children: [
-            staticCard( "Avg Order Value ", "€10", Icons.trending_up, Colors.blue),
-              const SizedBox(width: 16,)
-            , staticCard( "Active orders ", "10", Icons.pending_actions, Colors.orange),
-          ],)
-        ],
+              staticCard("Total Revenue", "€5000", Icons.payment, Colors.green),
+              
+            ],),
+
+            SizedBox(height: 30,),
+
+            Row(
+              children: [
+                staticCard("Avg Order value", "€60", Icons.trending_up, Colors.blue),
+                const SizedBox(width: 12,),
+                staticCard("Total Customers Served", "200", Icons.people, Colors.purple),
+                ],
+              ),
+
+              SizedBox(height: 30,),
+
+              Row(
+                children: [
+                staticCard("Returning Customers", "50", Icons.person_outline, Colors.teal),
+                          const SizedBox(width: 12,),
+
+                staticCard("Avg Order time ", "13:40", Icons.schedule, Colors.deepOrange),
+
+              ],
+            ),
+            SizedBox(height: 30,),
+            Row(children: [
+
+              staticCard("Best month", "Jan", Icons.calendar_month, Colors.blue),
+                              const SizedBox(width: 12,),
+
+              staticCard("Worst month", "July", Icons.calendar_today, Colors.red),
+            ],)
+
+          ],
+        ),
       ),
       
     );
